0987d918557220426d7dde947a3e8766ac52121d
namchokGithub <pech4751@gmail.com>
2020-08-04 17:31:06 +0700

    show data from database

--- InsertRecordMvcCore/Controllers/ManagementController.cs
+++ InsertRecordMvcCore/Controllers/ManagementController.cs
@@ -1,15 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentAssertions.Common;
 using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 
 namespace InsertRecordMvcCore.Controllers
 {
     public class ManagementController : Controller
     {
+
+        private readonly IConfiguration configuration;
+
+        public ManagementController(IConfiguration config)
+        {
+            this.configuration = config;
+        }
+
         public IActionResult Index()
         {
+            string connectionstring = configuration.GetConnectionString("DefaultConnect");
+
+            SqlConnection connection = new SqlConnection(connectionstring);
+
+            connection.Open();
+
+            string sql = "Select ID, Username, Password from account";
+
+            SqlCommand cmSelect = new SqlCommand(sql, connection);
+            var user = cmSelect.ExecuteReader();
+
+            List<Models.AccountClass> userlist = new List<Models.AccountClass>();
+
+            while (user.Read())
+            {
+
+                userlist.Add(
+                    new Models.AccountClass()
+                    {
+                        ID = (int)user["ID"],
+                        Username = user["Username"].ToString(),
+                        Password = user["Password"].ToString()
+                    }
+                );
+
+            }
+
+            ViewData["user"] = userlist;
+
+            connection.Close();
+
             return View();
         }
     }
