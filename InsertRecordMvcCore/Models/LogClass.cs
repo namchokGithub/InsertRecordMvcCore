9f780c4270e0f9c119c6a80673a002be46c205c3
namchokGithub <pech4751@gmail.com>
2020-08-06 13:59:30 +0700

    add log class

---
+++ InsertRecordMvcCore/Models/LogClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+/*
+ * Namespace: InsertRecordMvcCore
+ * Name class: LogClass
+ * Author: Namchok
+ */
+
+namespace InsertRecordMvcCore.Models
+{
+    public class LogClass
+    {
+        [Key]
+        public int log_Id { get; set; }
+        [Required]
+        public string log_date { get; set; }
+        [Required]
+        public string log_detail { get; set; }
+    }
+}
