a43d92f37c8dfdb955492df27fc4ac0dad01c7a7
namchokGithub <pech4751@gmail.com>
2020-08-11 10:05:43 +0700

    update display name in model

--- InsertRecordMvcCore/Models/AccountClass.cs
+++ InsertRecordMvcCore/Models/AccountClass.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Bogus.DataSets;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Microsoft.Azure.Management.Sql.Fluent.Models;
+using Nest;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -15,18 +19,23 @@ namespace InsertRecordMvcCore.Models
     public class AccountClass
     {
         [Key]
+        [Display(Name="ID")]
         public int acc_Id { get; set; }
 
         [Required]
+        [Display(Name="Name")]
         public String acc_firstname { get; set; }   // ชื่อจริง 
 
         [Required]
+        [Display(Name="Lastname")]
         public String acc_lastname { get; set; }    // นามสกุล
 
         [Required]
+        [Display(Name="Username")]
         public String acc_user { get; set; }        // อีเมลหรือเบอร์โทรศัพท์
 
         [Required]
+        [Display(Name="Password")]
         public String acc_password { get; set; }    // รหัสผ่าน
 
         [Required]
@@ -36,9 +45,11 @@ namespace InsertRecordMvcCore.Models
         public char acc_IsActive { get; set; }  // ตรวจสอบสถานะว่าบัญชีนี้ใช้งานได้หรือไม่
 
         [Required]
+        [Display(Name="Type of user")]
         public int acc_ro_Id { get; set; } // Foreign key ของตางราง Role
 
         [Required]
+        [Display(Name="Type of account")]
         public int acc_ta_Id { get; set; } // Foreign key ของตางราง Type_Account
     }
 }
